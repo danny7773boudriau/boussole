29755d2baa8a657a7c8ba8690b61ccf5ddc19013
danny7773boudriau <73191165+danny7773boudriau@users.noreply.github.com>
2020-11-24 14:49:49 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,35 @@
-let direction_boussole = 0
+let degré_0_nord = 0
+let degré = 0
 input.onButtonPressed(Button.A, function () {
-    if (true) {
+    let nord = 0
+    if (nord < input.compassHeading()) {
+        control.raiseEvent(
+        EventBusSource.MICROBIT_ID_BUTTON_A,
+        EventBusValue.MES_DEVICE_ORIENTATION_LANDSCAPE
+        )
+        control.raiseEvent(
+        input.magneticForce(Dimension.Strength),
+        input.rotation(Rotation.Pitch)
+        )
         input.calibrateCompass()
-        direction_boussole = input.compassHeading()
+        degré_0_nord = 0
+        degré = 360
+        basic.showArrow(ArrowNames.North)
         basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
             `)
     }
 })
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
