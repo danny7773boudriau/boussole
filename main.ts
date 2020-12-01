db4452b81908b98e82e396e490b2edcb22b09576
danny7773boudriau <73191165+danny7773boudriau@users.noreply.github.com>
2020-12-01 14:49:57 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,24 @@
-let degré_0_nord = 0
 let degré = 0
-input.onButtonPressed(Button.A, function () {
-    let nord = 0
-    if (nord < input.compassHeading()) {
-        control.raiseEvent(
-        EventBusSource.MICROBIT_ID_BUTTON_A,
-        EventBusValue.MES_DEVICE_ORIENTATION_LANDSCAPE
-        )
-        control.raiseEvent(
-        input.magneticForce(Dimension.Strength),
-        input.rotation(Rotation.Pitch)
-        )
-        input.calibrateCompass()
-        degré_0_nord = 0
-        degré = 360
-        basic.showArrow(ArrowNames.North)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    }
-})
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    degré = input.compassHeading()
+    if (degré < 45) {
+        basic.showString("N")
+        if (degré < 135) {
+            basic.showString("E")
+            if (degré < 225) {
+                basic.showString("S")
+                if (degré < 315) {
+                    basic.showString("W")
+                } else {
+                    basic.showString("N")
+                }
+            } else {
+            	
+            }
+        } else {
+        	
+        }
+    } else {
+    	
+    }
 })
